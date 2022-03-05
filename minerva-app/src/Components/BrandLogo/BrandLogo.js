a3423a2e25e95e0fdb9f36dfe436fcb4cbfacae0
Andres Echeverria <andres@brandpro.cl>
2022-03-04 22:22:51 -0300

    estilos navbar

--- minerva-app/src/Components/BrandLogo/BrandLogo.js
+++ minerva-app/src/Components/BrandLogo/BrandLogo.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './BrandLogo.css'
-import logo from './Isotipoclaro.png'
+import logo from './isotipoClaro.png'
+// import { SvgComponent as Logo } from './BrandLogo';
+// import SvgComponent from './SvgComponent'
+// import Logo from './BrandLogo'
+
 
 function BrandLogo() {
   return (
     <img src={logo} className='brandLogo' alt=""/>
+  //  <Logo />
+  // <SvgComponent />
+  // <Logo />
   )
 }
 
