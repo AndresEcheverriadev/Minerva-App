d1a40ef855cb37bce89834a1145a4c8c20d20cbf
Andres Echeverria <andres@brandpro.cl>
2022-03-04 23:04:57 -0300

    estilo cart

--- minerva-app/src/App.js
+++ minerva-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
+import ItemListContainer from './Containers/ItemListContainer';
 
 function App() {
   return (
     <div className="App">  
-      <NavBar />  
+      <NavBar /> 
+      <ItemListContainer greeting='Hello from Minerva' /> 
     </div>
     
   );
