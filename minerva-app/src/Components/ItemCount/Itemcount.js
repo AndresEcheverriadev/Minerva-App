c9aaf38e2e9e64218ca68d985e40fcf7fa805246
Andres Echeverria <andres@brandpro.cl>
2022-03-14 00:45:07 -0300

    item container design ok

--- minerva-app/src/Components/ItemCount/Itemcount.js
+++ minerva-app/src/Components/ItemCount/Itemcount.js
@@ -2,11 +2,11 @@ import {useState} from 'react';
 import './ItemCount.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
+import ImageProductos from '../../Assets/ImgsProductos1.png'
 export default 
 
 function Itemcount ( {stock, initial, onAdd} ) {
 const [count, setCount] = useState(initial); 
-// const stock = useState(11);
 
 const handlerCountPlus = () => {
   if (count < stock) {
@@ -29,15 +29,18 @@ const addtoBag = () => {
 
 
   return (
+    <>
     <div className='itemCountContainer'>
-      <ButtonGroup aria-label="Basic example" className='btnQuantity'>
-        <Button variant="secondary" onClick={() => handlerCountMinus()}>-1</Button>
-        <Button variant="secondary" disabled>{count}</Button>
-        <Button variant="secondary" onClick={() => handlerCountPlus()}>+1</Button>
-    </ButtonGroup>
-    <Button variant="outline-light" className='btnAddOn'onClick={() => addtoBag()}>Agregar a la compra</Button>
-  </div>
-    
+      <h6 className='itemCountTitle'>Anillo Zafiro Bodicea</h6>
+      <div className='itemCountImgContainer'> <img alt='' src={ImageProductos} className='imageProduct'></img> </div>
+      <ButtonGroup aria-label="Basic example" className='itemCountBtnQuantityContainer'>
+        <Button variant="secondary" className='itemCountBtnQuantity' onClick={() => handlerCountMinus()}>-1</Button>
+        <Button variant="secondary" className='itemCountBtnQuantity' disabled>{count}</Button>
+        <Button variant="secondary" className='itemCountBtnQuantity' onClick={() => handlerCountPlus()}>+1</Button>
+      </ButtonGroup>
+      <Button variant="outline-light" className='itemCountBtnAddOn'onClick={() => addtoBag()}>Agregar a la compra</Button>
+    </div>
+  </>
     
   )
 }
