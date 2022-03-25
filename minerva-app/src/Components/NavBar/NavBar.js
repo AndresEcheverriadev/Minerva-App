93ccac109fbd4cb5fd36fa2404a71fe7603110fd
Andres Echeverria <andres@brandpro.cl>
2022-03-24 23:46:30 -0300

    Primera Entrega OK

--- minerva-app/src/Components/NavBar/NavBar.js
+++ minerva-app/src/Components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import './Navbar.css'
-import { NavLinkc } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Navbar  from "react-bootstrap/Navbar"
 import Container  from "react-bootstrap/Container"
 import Nav  from "react-bootstrap/Nav"
